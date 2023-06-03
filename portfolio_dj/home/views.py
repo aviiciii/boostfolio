fd25b34f19316f49cb609cfa52672c853969dd02
aviiciii <laavesh1@gmail.com>
2023-06-03 13:35:06 +0530

    working on adding project

--- portfolio_dj/home/views.py
+++ portfolio_dj/home/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home/index.html', {})
 
-
+@csrf_exempt
 def portfolio_input(request):
-    return render(request, 'home/inputpage.html', {})
+
+    if request.method == 'POST':
+        print(request.POST)
+        return redirect('portfolio_input')
+
+
+    return render(request, 'home/input_portfolio.html', {})
 
 def job_input(request):
     return render(request, 'home/jobinput.html', {})
